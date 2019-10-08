fc1d6b1e719a8e1be618ac3aabbf118fc017f2f5
Tanner Haley <Haley.Tanner@cryolife.com>
2019-10-08 09:26:30 -0400

    End of section 7

--- DatingApp-SPA/src/app/navbar/navbar.component.ts
+++ DatingApp-SPA/src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { NgForm } from '@angular/forms';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +12,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavbarComponent implements OnInit {
   isLoggedIn = false;
   model: any = {};
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,6 +22,8 @@ export class NavbarComponent implements OnInit {
       this.alertify.success('Logged In success!');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
     form.reset();
   }
@@ -32,5 +35,6 @@ export class NavbarComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged Out');
+    this.router.navigate(['/home']);
   }
 }
